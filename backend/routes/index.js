d00b073a81a1b9b9ea3aa2ac98f7f3211683ff18
luisferdev11 <lrodriguezd1900@alumno.ipn.mx>
2022-01-19 22:14:26 -0600

    actualizando el cors

--- backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const users = require('../apiServices/users/routes');
-
-const router = express.Router();
-
-router.use('/users', users);
-
-module.exports = router;
