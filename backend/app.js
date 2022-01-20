873eb351218d4ec1695681841a2d979987ff02c9
luisferdev11 <lrodriguezd1900@alumno.ipn.mx>
2022-01-19 21:15:14 -0600

    cors added

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,19 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const routes = require('./routes');
-const { error404Handler, errorHandler } = require('./middleware');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const routes = require("./routes");
+const { error404Handler, errorHandler } = require("./middleware");
+const cors = require("cors");
 
 const app = express();
-app.use(logger('dev'));
+
+app.use(cors());
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/v1', routes);
+app.use("/v1", routes);
 
 app.use(error404Handler);
 app.use(errorHandler);
